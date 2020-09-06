f9a701e358043c4342f0f10320c1ecac7890cb86
Mikel Bishop <mdrew8@gmail.com>
2020-09-06 18:47:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,13 @@
 class Song 
   
   attr_accessor :name, :artist, :genre
-  count = 0 
+  @@count = 0 
   
   def initialize (name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre 
-    count += 1 
+    @@count += 1 
   end 
   
   def count
